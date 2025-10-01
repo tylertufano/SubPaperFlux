942caec6d779cfc69e773043ec16e9a1541c550c
Tyler <69593911+tylertufano@users.noreply.github.com>
2025-09-30 21:31:00 -0500

    chore: remove e2e test harness

--- web/e2e/playwright.setup.ts
+++
@@ -1,9 +0,0 @@
-import type { FullConfig } from '@playwright/test'
-
-const DEFAULT_BASE_URL = 'http://127.0.0.1:8000'
-
-export default async function globalSetup(_config: FullConfig) {
-  if (!process.env.NEXT_PUBLIC_API_BASE) {
-    process.env.NEXT_PUBLIC_API_BASE = DEFAULT_BASE_URL
-  }
-}
